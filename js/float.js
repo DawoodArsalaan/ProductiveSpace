04359dd6724a1da57a40e520395b67b534b83d6c
DawoodArsalaan <72990250+DawoodArsalaan@users.noreply.github.com>
2021-08-23 17:25:38 +0530

    Add files via upload

---
+++ js/float.js
@@ -0,0 +1,140 @@
+var btn = document.querySelector('#floatimg1');
+var btn1 = document.querySelector('#floatimg2');
+var btn2 = document.querySelector('#floatimg3');
+var btn3 = document.querySelector('#floatimg4');
+var btn4 = document.querySelector('#floatimg5');
+var btn5 = document.querySelector('#floatimg6');
+var btn6 = document.querySelector('#floatimg7');
+var btn7 = document.querySelector('#floatimg8');
+var btn8 = document.querySelector('#floatimg9');
+var btn9 = document.querySelector('#floatimg10');
+var song = document.getElementById('song');
+var song1 = document.getElementById('song1');
+var song2 = document.getElementById('song2');
+var song3 = document.getElementById('song3');
+var song4 = document.getElementById('song4');
+var song5 = document.getElementById('song5');
+var song6 = document.getElementById('song6');
+var song7 = document.getElementById('song7');
+var song8 = document.getElementById('song8');
+var song9 = document.getElementById('song9');
+btn.onclick = function() {
+    if(song.paused)
+    {
+        song.play();
+        btn.src = "media/pause.png"
+    }
+    else
+    {
+        song.pause();
+        btn.src = "media/play.png"
+    }
+}
+btn1.onclick = function() {
+    if(song1.paused)
+    {
+        song1.play();
+        btn1.src = "media/pause.png"
+    }
+    else
+    {
+        song1.pause();
+        btn1.src = "media/play.png"
+    }
+}
+btn2.onclick = function() {
+    if(song2.paused)
+    {
+        song2.play();
+        btn2.src = "media/pause.png"
+    }
+    else
+    {
+        song2.pause();
+        btn2.src = "media/play.png"
+    }
+}
+btn3.onclick = function() {
+    if(song3.paused)
+    {
+        song3.play();
+        btn3.src = "media/pause.png"
+    }
+    else
+    {
+        song3.pause();
+        btn3.src = "media/play.png"
+    }
+}
+btn4.onclick = function() {
+    if(song4.paused)
+    {
+        song4.play();
+        btn4.src = "media/pause.png"
+    }
+    else
+    {
+        song4.pause();
+        btn4.src = "media/play.png"
+    }
+}
+btn5.onclick = function() {
+    if(song5.paused)
+    {
+        song5.play();
+        btn5.src = "media/pause.png"
+    }
+    else
+    {
+        song5.pause();
+        btn5.src = "media/play.png"
+    }
+}
+btn6.onclick = function() {
+    if(song.paused)
+    {
+        song6.play();
+        btn6.src = "media/pause.png"
+    }
+    else
+    {
+        song6.pause();
+        btn6.src = "media/play.png"
+    }
+}
+btn7.onclick = function() {
+    if(song7.paused)
+    {
+        song7.play();
+        btn7.src = "media/pause.png"
+    }
+    else
+    {
+        song7.pause();
+        btn7.src = "media/play.png"
+    }
+}
+btn8.onclick = function() {
+    if(song8.paused)
+    {
+        song8.play();
+        btn8.src = "media/pause.png"
+    }
+    else
+    {
+        song8.pause();
+        btn8.src = "media/play.png"
+    }
+}
+btn9.onclick = function() {
+    if(song9.paused)
+    {
+        song9.play();
+        btn9.src = "media/pause.png"
+    }
+    else
+    {
+        song9.pause();
+        btn9.src = "media/play.png"
+    }
+}
---
+++ login.php
@@ -0,0 +1,40 @@
+<?php 
+
+include('connection.php');
+
+session_start();
+
+if(isset($_POST['signin']));
+{
+    $user_unsafe=$_POST['name'];
+    $pass_unsafe=$_POST['password'];
+
+    $name = mysqli_real_escape_string($con,$user_unsafe);
+    $password = mysqli_real_escape_string($con,$pass_unsafe);
+
+    $query=mysqli_query($con,"select * from registration where name='$name' and password='$password'")or die(mysqli_error($con));
+
+    $row=mysqli_fetch_array($query);
+
+         $name=$row['name'];
+         $counter=mysqli_num_rows($query);
+         $id=$row['id'];
+
+         if ($counter == 0)
+         {
+            echo "<script type='text/javascript'>alert('Invalid Username or Password!');
+            document.location='login.html'</script>";
+         }
+         else
+         {
+            $_SESSION['id']=$id;
+            $_SESSION['name']=$name;
+
+            echo "<script type='text/javascript'>alert('Logged In Successfully');
+            document.location='homelogged.html'</script>";
+         }
+
+}
+
+?>
+          
